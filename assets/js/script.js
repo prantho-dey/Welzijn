7320cd0e4a64ed5b624249a243f89195857e8e4d
Prantho <pranthodey89@gmail.com>
2021-04-27 00:23:36 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+(function ($) {
+
+    'use strict';
+
+	/*------------------------------------
+		Mobile Menu
+	--------------------------------------*/
+
+
+	/*-------------------------------------------
+	    Sticky Header
+	--------------------------------------------- */
+
+	let win = $(window);
+	let sticky_id = $(".header-area");
+	win.on('scroll', function () {
+		let scroll = win.scrollTop();
+		if (scroll < 245) {
+			sticky_id.removeClass("sticky-header");
+		} else {
+			sticky_id.addClass("sticky-header");
+		}
+	});
+
+
+	/*------------------------------------
+        scrollUp
+	--------------------------------------*/
+	$(window).scroll(function () {
+		if ($(this).scrollTop() !== 0) {
+			$('#scrollUp').fadeIn();
+		} else {
+			$('#scrollUp').fadeOut();
+		}
+    });
+    
+	$('#scrollUp').on('click', function () {
+		$("html, body").animate({scrollTop: 0}, 600);
+		return false;
+	});
+
+
+
+
+})(jQuery);
